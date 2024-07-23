d03c8a02ae1062ed5b6390e12088f3ab08e5b113
avinashb77 <94923838+avinashb77@users.noreply.github.com>
2024-07-23 17:26:06 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'In the Game of election you are getting my vote of love without a campaign My Thangooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'
+    return 'In the Game of election you are grabbing  my vote of love without a campaign My Thangooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
