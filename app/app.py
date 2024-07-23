4b47cb952c91c27b5378eea823d12bfcf77c4290
avinashb77 <94923838+avinashb77@users.noreply.github.com>
2024-07-23 15:37:43 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Devops and CICD Jenkins Application for Demo Purpose Do14WD batch 7'
+    return 'Is your body A MAP--? Becasue am getting lost in every Curve'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
