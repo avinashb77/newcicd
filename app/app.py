7b25ad2038a947889f0d5c6f3748567438f508a6
avinashb77 <94923838+avinashb77@users.noreply.github.com>
2024-07-23 15:43:46 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Is your body A MAP--? Becasue am getting lost in every Curve'
+    return 'Is your body A MAP--? Becasue am getting lost in every Curve Thangooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
